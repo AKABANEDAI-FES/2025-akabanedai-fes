4bcc1272552f795706e2243ae46cc2b6b63357a7
yu7400ki <yu7400ki@gmail.com>
2025-09-24 00:14:53 +0900

    css-modules-kitを導入

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import("stylelint").Config} */
 export default {
-  extends: ["stylelint-config-standard"],
+  extends: [
+    "stylelint-config-standard",
+    "@css-modules-kit/stylelint-plugin/recommended",
+  ],
 };
