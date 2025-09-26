97d716b7da4276024553c02b66f6c2c6df897666
yu7400ki <yu7400ki@gmail.com>
2025-09-26 22:02:28 +0900

    フッターを作成

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,36 @@
+import InstagramBrandIcon from "@/assets/instagram-brand.svg";
+import XBrandIcon from "@/assets/x-brand.svg";
+import { Text, text } from "../ui/text";
+import styles from "./footer.module.css";
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.contact}>
+        <Text className={styles.email}>
+          <span>お問い合わせ先</span>
+          <span>support[at]akabanedai-fes.com</span>
+        </Text>
+        <div className={styles.sns}>
+          <a
+            className={text()}
+            href="https://x.com/akabanedaifes/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <XBrandIcon className={styles.icon} />
+          </a>
+          <a
+            className={text()}
+            href="https://www.instagram.com/akabanedaifes/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <InstagramBrandIcon className={styles.icon} />
+          </a>
+        </div>
+      </div>
+      <Text>&copy;2025 赤羽台祭実行委員会</Text>
+    </footer>
+  );
+}
