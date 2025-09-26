5b6830e47d727360350201bf557d7a6f8970b814
yu7400ki <yu7400ki@gmail.com>
2025-09-26 21:54:06 +0900

    keyframes-name-patternルールをstylelint設定に追加

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -7,5 +7,6 @@ export default {
   rules: {
     "selector-class-pattern": "^[a-z][a-zA-Z0-9]+$",
     "selector-id-pattern": "^[a-z][a-zA-Z0-9]+$",
+    "keyframes-name-pattern": "^[a-z][a-zA-Z0-9]+$",
   },
 };
