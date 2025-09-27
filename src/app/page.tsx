b1a596300951aa07e018757405cdf707abb000ca
yu7400ki <yu7400ki@gmail.com>
2025-09-28 04:50:34 +0900

    ページコンポーネントを追加し、スタイルを適用

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
+import { Container } from "@/components/ui/container";
+import { Access } from "./_components/access";
+import { Banner } from "./_components/banner";
+import { Parallax } from "./_components/parallax";
+import { Theme } from "./_components/theme";
+import styles from "./page.module.css";
+
 export default function Home() {
-  return null;
+  return (
+    <Parallax.Root>
+      <div className={styles.overlay}>
+        <Header />
+      </div>
+      <Parallax.Layer />
+      <Parallax.Content>
+        <Banner />
+        <Container as="main" className={styles.container}>
+          <Theme />
+          <Access />
+        </Container>
+        <Footer />
+      </Parallax.Content>
+    </Parallax.Root>
+  );
 }
