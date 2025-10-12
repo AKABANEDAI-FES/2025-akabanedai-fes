b62a15e8708e0106859c6515a377aea749c466d9
yu7400ki <yu7400ki@gmail.com>
2025-10-13 05:23:56 +0900

    フォーマット

--- src/app/_components/banner/banner.tsx
+++ src/app/_components/banner/banner.tsx
@@ -1,7 +1,7 @@
 import { VisuallyHidden } from "@/components/ui/visually-hidden";
+import styles from "./banner.module.css";
 import FirstView from "./first-view.svg";
 import MobileFirstView from "./mobile-first-view.svg";
-import styles from "./banner.module.css";
 
 export function Banner() {
   return (
@@ -13,8 +13,16 @@ export function Banner() {
         <time dateTime="2025-11-03">11/03(月)</time>
         <time dateTime="10:00">10:00</time>～<time dateTime="18:00">18:00</time>
       </VisuallyHidden>
-      <MobileFirstView aria-hidden="true" role="presentation" className={styles.mobile} />
-      <FirstView aria-hidden="true" role="presentation" className={styles.desktop} />
+      <MobileFirstView
+        aria-hidden="true"
+        role="presentation"
+        className={styles.mobile}
+      />
+      <FirstView
+        aria-hidden="true"
+        role="presentation"
+        className={styles.desktop}
+      />
     </div>
   );
 }
