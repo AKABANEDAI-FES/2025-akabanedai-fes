4c7bd763f052feab5a899b63658d1db5fe1b20c8
yu7400ki <yu7400ki@gmail.com>
2025-10-29 23:46:26 +0900

    ヘッダーのナビゲーションメニューを実装

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,18 +1,14 @@
+import { NavigationMenu } from "@base-ui-components/react/navigation-menu";
 import clsx from "clsx";
 import Link from "next/link";
 import type { JSX } from "react";
 import SymbolIcon from "@/assets/symbol.svg";
+import { BlurredBox } from "../ui/blurred-box";
 import { Text } from "../ui/text";
 import styles from "./header.module.css";
+import { navigation } from "./nav";
 
 export type Props = Omit<JSX.IntrinsicElements["div"], "children">;
-
-const navItems = [
-  { label: "企画情報" },
-  { label: "赤羽台祭について" },
-  { label: "公式コンテンツ" },
-  { label: "ご来場の皆様へ" },
-];
 
 export function Header({ className, ...props }: Props) {
   return (
@@ -21,19 +17,58 @@ export function Header({ className, ...props }: Props) {
         <Link href="/" aria-label="赤羽台祭 ホームへ">
           <SymbolIcon className={styles.symbol} />
         </Link>
-        <nav className={styles.navigation}>
-          <ul className={styles.navigationList}>
-            {navItems.map((item) => (
-              <li
-                key={item.label}
-                className={styles.navigationItem}
-                aria-disabled
-              >
-                <Text as="span">{item.label}</Text>
-              </li>
+        <NavigationMenu.Root className={styles.navigation}>
+          <NavigationMenu.List className={styles.navigationList}>
+            {navigation.map((nav) => (
+              <NavigationMenu.Item key={nav.label}>
+                <NavigationMenu.Trigger>
+                  <Text as="span">{nav.label}</Text>
+                </NavigationMenu.Trigger>
+                <NavigationMenu.Content className={styles.menuContent}>
+                  <BlurredBox as="ul" className={styles.menuNavList}>
+                    {nav.items.map((item) => (
+                      <Text as="li" key={item.label}>
+                        {item.href ? (
+                          <NavigationMenu.Link
+                            key={item.label}
+                            href={item.href}
+                            className={styles.menuNavLink}
+                            render={<Link href="#" />}
+                          >
+                            {item.label}
+                          </NavigationMenu.Link>
+                        ) : (
+                          // biome-ignore lint/a11y/useSemanticElements: リンクでないメニュー項目のため
+                          <Text
+                            as="span"
+                            role="link"
+                            aria-disabled="true"
+                            className={styles.menuNavLink}
+                          >
+                            {item.label}
+                          </Text>
+                        )}
+                      </Text>
+                    ))}
+                  </BlurredBox>
+                </NavigationMenu.Content>
+              </NavigationMenu.Item>
             ))}
-          </ul>
-        </nav>
+          </NavigationMenu.List>
+          <NavigationMenu.Portal>
+            <NavigationMenu.Positioner
+              className={styles.positioner}
+              side="bottom"
+              align="start"
+              alignOffset={-32}
+              collisionAvoidance={{ side: "none" }}
+            >
+              <NavigationMenu.Popup>
+                <NavigationMenu.Viewport />
+              </NavigationMenu.Popup>
+            </NavigationMenu.Positioner>
+          </NavigationMenu.Portal>
+        </NavigationMenu.Root>
       </header>
     </div>
   );
