66fab43e5fb5ae1407f95aa55e06053bb0e88b43
s1f10250103 <s1f102501035@iniad.org>
2025-10-30 18:42:58 +0900

    タイムテーブルを作成しました

---
+++ src/app/timetable/page.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import Image from "next/image";
+import { BlurredBox } from "@/components/ui/blurred-box";
+import { Container } from "@/components/ui/container";
+import { Heading } from "@/components/ui/heading";
+import { StarDecoration } from "@/components/ui/star-decoration";
+import { Text } from "@/components/ui/text";
+import styles from "./page.module.css";
+import timetable from "./timetable.webp";
+
+export const metadata: Metadata = {
+  title: "タイムテーブル",
+};
+
+export default function () {
+  return (
+    <Container as="main" className={styles.mainContent}>
+      <StarDecoration>
+        <Heading as="h1">タイムテーブル</Heading>
+      </StarDecoration>
+
+      <BlurredBox as="section" className={styles.section}>
+        <Image
+          src={timetable}
+          alt="タイムテーブル"
+          className={styles.timetable}
+        />
+        <Text>
+          ※タイムテーブルは変更になる場合がございます。ご了承ください。
+        </Text>
+        <Text>
+          ※11/2のINIAD卒業生対象イベントはINIAD HUB-1の2階学生食堂で開催しています。
+        </Text>
+      </BlurredBox>
+    </Container>
+  );
+}
