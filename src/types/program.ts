58721167f30ba5d012baab39d60a563b9ca41e92
1F10240256 <s1f102402563@iniad.org>
2025-10-30 11:01:34 +0900

    型定義を追加

---
+++ src/types/program.ts
@@ -0,0 +1,12 @@
+export type Program = {
+  title: string;
+  officialtitle: string;
+  organization: string;
+  description: string;
+  placement: string;
+  contest: string | number;
+  where: string | null;
+  logo: string;
+  tags: string[];
+  caution: string[];
+};
---
+++ src/utils/program.ts
@@ -0,0 +1,17 @@
+import programs from "@/assets/programs.json";
+import type { Program } from "@/types/program";
+
+export function getPrograms(): Program[] {
+	return programs.map((program) => ({
+		title: program.企画名,
+    officialtitle: program.正式企画名,
+    organization: program.団体名,
+		description: program.紹介文,
+		placement: program.教室 ?? "",
+		contest: program.企画コンテスト?? "",
+    where: program.where,
+    logo: `/logo/${program.logo}`,
+    tags: program.タグ,
+    caution : program.caution?? "",
+	}));
+}
