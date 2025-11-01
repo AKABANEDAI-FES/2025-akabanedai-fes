a546677fd832d2b337b80da06ed5e3fa7182fd0d
yu7400ki <yu7400ki@gmail.com>
2025-11-02 01:46:36 +0900

    パンフレットページ

---
+++ src/app/(routes)/pamphlet/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import { Container } from "@/components/ui/container";
+import { Heading } from "@/components/ui/heading";
+import { LinkButton } from "@/components/ui/link-button";
+import { StarDecoration } from "@/components/ui/star-decoration";
+import cover from "./cover.webp";
+import styles from "./page.module.css";
+
+export default function PamphletPage() {
+  return (
+    <Container as="main" className={styles.container}>
+      <StarDecoration>
+        <Heading as="h1">パンフレット</Heading>
+      </StarDecoration>
+      <Image src={cover} alt="パンフレット表紙" className={styles.cover} />
+      <LinkButton href="/9thpamphlet.pdf" download>
+        ダウンロード（24.2MB）
+      </LinkButton>
+    </Container>
+  );
+}
